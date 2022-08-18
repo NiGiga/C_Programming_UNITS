c9865bfd7d5601b5182a9650cafccf0cb218db02
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2022-08-18 18:08:09 +0000

    done

---
+++ .ccls-cache/@home@runner@PitifulSaddlebrownConditionals/ex3.c
@@ -0,0 +1,42 @@
+/*Esercizio 9.3. Si scriva un programma capace di compiere le 4 operazioni
+(somma, sottrazione, moltiplicazione e divisione) tra due numeri reali inseriti
+da tastiera. Dopo che sono stati inseriti i due numeri, detti a e b, il programma
+dovrà visualizzare i quattro valori a + b, a − b, ab, a/b. Si ipotizzi che b ̸= 0.
+Esempio di output.
+> Inserisci il primo numero (a): 16
+> Inserisci il secondo numero (b): 4
+> La somma a + b è: 20
+> La differenza a - b è: 12
+> Il prodotto a * b è: 64
+> La divisone a / b è: 4*/
+
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b, sum, dif, prod, div;
+    
+
+    printf("Inserisci il primo numero:");
+    scanf("%d", &a);
+
+    printf("Inserisci il secondo numero:");
+    scanf("%d", &b);
+
+    sum = a + b;
+    
+    dif = a - b;
+
+    prod = a * b;
+
+    div = a / b;
+
+    printf("La somma a + b è: %d\n", sum);
+    printf("La differenza a - b è: %d\n", dif);
+    printf("La prod a * b è: %d\n", prod);
+    printf("La divisione a / b è: %d\n", div);
+
+    return 0;
+  
+}
