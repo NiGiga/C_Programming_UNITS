714eff670a9e295c8a01847fc90421a9c3a99be8
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2022-08-27 12:13:43 +0000

    commentato

--- esercitazione1/es1.1.c
+++ esercitazione1/es1.1.c
@@ -12,28 +12,33 @@ int main() {
     printf("Inserisci la variabile x\n");
     scanf("%d", &x);
 
-
+    // chiamo la funzione
     int succesion = succesione(n);
 
-    printf("La sommatoria di x^k/k! con k che va da 0 a n, dove x = %d e n =%d "
-         "e' = %d",
-         x, n, succesion);
+    printf("La sommatoria di x^k/k! con k che va da 0 a n, dove x = %d e n =%d e' = %d",x, n, succesion);
 
     return 0;
 }
-
+//f unzione per il calcolo del fattoriale in maniera ricorsiva
 int fattoriale(int num) {
 
+    // determino il caso base
     if (num == 0 | num == 1)return 1;
+
+    // chiamata ricorsiva
     return num * fattoriale(num - 1);
 }
 
-
+// funzione per il calcolo della succesione in maniera iterativa
 int succesione(int n)
 {
     int s_n_1 = 1;
     for(int i=1; i<=n; i++){
+
+        // calcolo il primo chiamando la funzione fattoriale pe il fattoriale
         int s_n = (x^n)/fattoriale(n);
+
+        // aggiorno il valore del caso base
         s_n_1 = s_n;
     }
     return s_n_1;
