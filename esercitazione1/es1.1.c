e8e2331ba3359f6d9d8dfb098a7890ea3d6584c6
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2022-08-28 11:46:00 +0000

    done

--- esercitazione1/es1.1.c
+++ esercitazione1/es1.1.c
@@ -1,46 +1,83 @@
+/* Esercitazione 1: programma per il calcolo della succesione di una funzione di sommatoria, x^n/k! con k che va da 0 a n. Si vuole calcolare l'ennesimo termine della succesione e stamparlo a schermo con i singoli termini della succesione, partendo da uno user input di x e n.*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
-int fattoriale(int num);
-int succesione(int n);
-int n = 0, x = 0;
+// creo la funzione per il calcolo del fattoriale in maniera ricorsiva
+int fattoriale(int k)
+{
+    if(k == 0)
+        return 1;
+    return k * fattoriale(k - 1);
+}
 
-// linea di comando
-int main() {
-    
-    printf("Inserisci la variabile n\n");
+// creo la funzione per il calcolo della potenza in maniera ricorsiva
+float potenza(float a, int b)
+{
+    if(b == 0)
+        return 1;
+    return a * potenza(a, b - 1);
+}
+
+// creo la funzione per il calcolo della sommatoria in maniera iterativa
+float sommatoria_iterativa(float x, int n)
+{
+    float somma = 0;
+    for(int i = 0; i <= n; i++)
+    {
+        somma = somma + ((float)potenza(x, i) / (float)fattoriale(i));
+    }
+    return somma;
+}
+
+// creo la funzione per il calcolo della sommatoria in maniera ricorsiva
+float sommatoria_ricorsiva(float x, int n)
+{
+    if(n == 0)
+        return 1.0;
+    return (potenza(x, n) / (float)fattoriale(n)) + sommatoria_ricorsiva(x, n - 1);
+}
+
+// creo la funzione per allocare trammite un ciclo for ogni 
+// elemento della succesione nellamemoria dinamica
+float* array_somma(float x, int n)
+{
+    float* a = malloc(sizeof(float) * n);
+
+    for(int i = 0; i < n; i++)
+    {
+        a[i] = sommatoria_iterativa(x, i);
+    }
+
+    return a;
+}
+
+// linea di comando 
+int main()
+{
+
+    // richiedo i valori in input
+    float x;
+    int n;
+    printf("Inserisci il valore di x\n");
+    scanf("%f", &x);
+    printf("Inserisci il valore di n\n");
     scanf("%d", &n);
-    printf("Inserisci la variabile x\n");
-    scanf("%d", &x);
 
-    // chiamo la funzione
-    int succesion = succesione(n);
+    // creo la lista linkata
+    float* p;
+    p = array_somma(x, n);
 
-    printf("La sommatoria di x^k/k! con k che va da 0 a n, dove x = %d e n =%d e' = %d",x, n, succesion);
+    // stampo a schermo la succesione(il contenuto della lista) 
+    // e la sommatoria della succesione(sommatoria_ricorsiva/iterativa)
+    printf("[");
+    for(int i = 0; i < 10; i++)
+        printf("%.3f ", p[i]);
+    printf("]\n");
+    free(p);
+    float sommatoria = sommatoria_iterativa( x, n);
+    printf("La sommatoria della succesione e': %f\n", sommatoria);
 
     return 0;
-}
-//f unzione per il calcolo del fattoriale in maniera ricorsiva
-int fattoriale(int num) {
-
-    // determino il caso base
-    if (num == 0 | num == 1)return 1;
-
-    // chiamata ricorsiva
-    return num * fattoriale(num - 1);
-}
-
-// funzione per il calcolo della succesione in maniera iterativa
-int succesione(int n)
-{
-    int s_n_1 = 1;
-    for(int i=1; i<=n; i++){
-
-        // calcolo il primo chiamando la funzione fattoriale pe il fattoriale
-        int s_n = (x^n)/fattoriale(n);
-
-        // aggiorno il valore del caso base
-        s_n_1 = s_n;
-    }
-    return s_n_1;
 }
 
